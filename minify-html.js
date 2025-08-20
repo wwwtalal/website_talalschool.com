f4b06cad5be4e4a273ae4cc8a7e43435e2b69b3b
Talal <wwwtalal@gmail.com>
2025-08-20 11:37:15 +0300

    added minify for css. replaced riyal with new symble

--- minify-html.js
+++ minify-html.js
@@ -1,5 +1,6 @@
 const { minify } = require('html-minifier-terser');
 const fs = require('fs');
+const path = require('path');
 
 const html = fs.readFileSync('src/index.html', 'utf8');
 
@@ -16,3 +17,20 @@ minify(html, {
   fs.writeFileSync('docs/index.html', result);
   console.log('HTML minified successfully!');
 });
+
+// Copy fonts from src/fonts to docs/fonts
+
+const srcFontsDir = path.join('src', 'fonts');
+const destFontsDir = path.join('docs', 'fonts');
+
+if (fs.existsSync(srcFontsDir)) {
+  fs.mkdirSync(destFontsDir, { recursive: true });
+  fs.readdirSync(srcFontsDir).forEach(file => {
+    const srcFile = path.join(srcFontsDir, file);
+    const destFile = path.join(destFontsDir, file);
+    fs.copyFileSync(srcFile, destFile);
+  });
+  console.log('Fonts copied successfully!');
+} else {
+  console.log('No fonts directory found to copy.');
+}
